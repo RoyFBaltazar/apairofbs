1919f52d9aef50a4e5d92b984395ff3ff8a444a8
RoyFBaltazar <94885352+RoyFBaltazar@users.noreply.github.com>
2022-02-21 20:40:52 -0500

    player added

--- src/pages/podcast.js
+++ src/pages/podcast.js
@@ -5,11 +5,23 @@ const Podcast =()=>{
         <div>
             <h1>Listen A Pair Of B's</h1>
     
-      <div className='imgLogo'><img src={logo}/></div>  
-
-
+      <div className='imgLogo'><img src={logo}/></div> 
+      <h2>Latest Episode:</h2> 
+      < div className='player'><iframe 
+      style={ {style:"border-radius:50px",
+      width:"50%" ,
+      height:"152",
+      frameBorder:"0"}}
+      
+      src="https://open.spotify.com/embed/show/1528ERPEo5YGpfTrMbMedr?utm_source=generator"
+      allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture;="
+      
+      
+      
+      ></iframe></div>
 
         </div>
     )
 }
+
 export default Podcast;
