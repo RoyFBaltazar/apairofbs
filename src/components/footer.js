9951fe21a00d46429420072c2660aed6e9ead523
RoyFBaltazar <94885352+RoyFBaltazar@users.noreply.github.com>
2022-02-22 09:22:29 -0500

    working on footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,48 @@
+import '../components/footer.css'
+
 const Footer=()=>{
 
     return(
-        <div>
-            <p>footer</p>
+        <div className="main-footer">
+            <div className="container">
+                <div className="row">
+
+                 Column1
+                 <div className="col">
+                     <h4> A Pair Of B's</h4>
+                     <ul className="list-unstyled">
+                        <li>Bronx, NY</li>
+                        <li></li>
+                     </ul>
+                     </div>   
+                 Column2 
+                 <div className="col">
+                     <h4> stuff</h4>
+                     <ul className="list-unstyled">
+                        <li>stuff</li>
+                        <li></li>
+                     </ul>
+                     </div>   
+                 Column3  
+                 <div className="col">
+                     <h4> one more stuff</h4>
+                     <ul className="list-unstyled">
+                        <li>Bronx, NY</li>
+                        <li></li>
+                     </ul>
+                     </div>  
+
+
+
+                </div>
+                <hr/>
+                 <div className="row">
+                     <p className="col-sm">
+                         | All right reserved| Privacy
+                     </p>
+                     </div>   
+            </div>
+            
         </div>
     )
 }
